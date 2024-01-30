deec0a7327e4a3b786fffbe97a1d010555d283f4
Aleksandr Antonov <antonoffa@list.ru>
2024-01-30 20:27:37 +0300

    Добавил несколько тестов

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def devision(a: int | float, b: int | float):
     return a/b
 
 def main():
-    d = devision(1, 2)
-    print(d)
+    for i, j in enumerate([1, 2, 3, 4], 1):
+        d = devision(i, j)
+        print(d)
 
 if __name__=="__main__":
     main()
