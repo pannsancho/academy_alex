0df544cd5101268d4b7059276afe03e923259f5b
Aleksandr Antonov <antonoffa@list.ru>
2024-01-30 20:47:50 +0300

    Исправил ошибка с делением на 0

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def devision(a: int | float, b: int | float):
+    if b == 0:
+        raise ZeroDivisionError("Деление на ноль невозможно!")
     return a/b
 
+
 def main():
-    for i, j in enumerate([1, 2, 3, 4], 1):
-        d = devision(i, j)
-        print(d)
+    for i, j in enumerate([1, 2, 3, 4], 0):
+        try:
+            d = devision(j, i)
+        except ZeroDivisionError as e:
+            print(e)
+        else:
+            print(d)
+
 
 if __name__=="__main__":
     main()
